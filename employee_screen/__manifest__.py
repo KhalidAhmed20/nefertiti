eb7e25514e3b4be43b69437dcd57af581cf4bb3e
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 13:41:14 +0200

    employee_screen

---
+++ employee_screen/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name' : 'employee_screen',
+    'description' : """employee screen""",
+    'version' : '1.0',
+    'category': 'Hr/Screen',
+    'depends' : ['base','hr','hr_payroll',],
+    'data': [
+        # "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/views_penality_group.xml",
+        "views/views_penality_sub.xml",
+        "views/views_pyslib_inh.xml",
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
