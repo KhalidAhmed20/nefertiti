0973d2666d7f9c0a2c2bfa3aa3040b4703dfc24d
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 14:50:18 +0200

    inventory_valuation_pivot

---
+++ inventory_valuation_pivot/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inventory Valuation Customization',
+    'summary': 'inventory_valuation_pivot',
+    'author': ' IBS',
+    'category': 'Inventory',
+    'depends': ['stock_account'],
+    'data': [
+        'views/stock_valuation_layer_views.xml',
+    ],
+
+}
