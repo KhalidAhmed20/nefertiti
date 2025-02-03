cb522f5aaa4d810f88b1d40bc97bbcf002e430d6
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 13:37:50 +0200

    edit_operation_type_vendor

---
+++ edit_operation_type_vendor/models/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class EditResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    picking_type_id = fields.Many2one('stock.picking.type', 'Picking Type', )
+
+
+class EditPurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    picking_type_id = fields.Many2one('stock.picking.type', 'Picking Type', )
+
+    @api.onchange('partner_id')
+    def get_picking_type_id(self):
+        for rec in self:
+            rec.picking_type_id = rec.partner_id.picking_type_id.id
