1e5e43ae8f72733143952f5a846bac3ba879c308
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 14:49:47 +0200

    inventory_inhance

---
+++ inventory_inhance/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "invoice inhance",
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+    # only loaded in demonstration mode
+
+}
