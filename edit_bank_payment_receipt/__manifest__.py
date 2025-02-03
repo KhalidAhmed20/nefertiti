7ef07dd1e1f704b3caaff3e910dfa91ab725edb1
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 13:35:48 +0200

    edit_bank_payment_receipt

---
+++ edit_bank_payment_receipt/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Edit Payment Receipt Report For Bank & Cash",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "IBS",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        'report/report_payment_receipt_templates.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
