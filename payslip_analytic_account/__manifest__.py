e0420545e23447cc181bb1d2da931948b52cb580
khalid <khalid.ahmed.eid18@gmail.com>
2025-02-03 14:58:26 +0200

    payslip_analytic_account

---
+++ payslip_analytic_account/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "payslip_analytic_account",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "IBS",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'hr_payroll_account'],
+
+}
